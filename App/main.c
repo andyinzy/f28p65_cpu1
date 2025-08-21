00a415e3754487e8ef2a465c73440e7170cd2f4b
william <william@sl.com>
2025-08-21 11:01:29 +0800

    feat:dual test

--- App/main.c
+++ App/main.c
@@ -63,6 +63,7 @@
 // Defines
 //
 #define DEVICE_GPIO_PIN_LED1    12
+#define DEVICE_GPIO_PIN_LED2    13
 
 //
 // Main
@@ -75,11 +76,50 @@ void main(void)
     InitSysCtrl();
 
     //
+    // Write the controller select setting into the appropriate field.
+    //
+    EALLOW;
+        MemCfgRegs.GSxMSEL.bit.MSEL_GS4 = 1U;
+    EDIS;
+
+    DevCfgRegs.BANKMUXSEL.bit.BANK3 = 3U;
+    DevCfgRegs.BANKMUXSEL.bit.BANK4 = 3U;
+
+    //
+    // Boot CPU2 core
+    //
+#ifdef _STANDALONE
+#ifdef _FLASH
+    //
+    //  Send boot command to allow the CPU02 application to begin execution
+    //
+    IPCBootCPU2(C1C2_BROM_BOOTMODE_BOOT_FROM_FLASH);
+#else
+    //
+    //  Send boot command to allow the CPU02 application to begin execution
+    //
+    IPCBootCPU2(C1C2_BROM_BOOTMODE_BOOT_FROM_RAM);
+#endif
+#endif
+
+    //
+    // Wait for IPC flag from CPU2
+    //
+	
+    //IPCLtoRFlagSet(IPC_FLAG0);
+
+    // while(IPCRtoLFlagBusy(IPC_FLAG0) == 0);
+    // IPCRtoLFlagAcknowledge(IPC_FLAG0);
+
+    //
     // Initialize GPIO and configure the GPIO pin as a push-pull output
     //
     InitGpio();
     GPIO_SetupPinMux(DEVICE_GPIO_PIN_LED1, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(DEVICE_GPIO_PIN_LED1, GPIO_OUTPUT, GPIO_PUSHPULL);
+
+    GPIO_SetupPinMux(DEVICE_GPIO_PIN_LED2, GPIO_MUX_CPU2, 0);
+    GPIO_SetupPinOptions(DEVICE_GPIO_PIN_LED2, GPIO_OUTPUT, GPIO_PUSHPULL);    
 
     //
     // Initialize PIE and clear PIE registers. Disables CPU interrupts.
